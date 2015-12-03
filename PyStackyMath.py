091b413dfa98e82330926d8d3248e7e13a6fa6f5
TanMath <TanMath@users.noreply.github.com>
2015-12-02 19:04:52 -0800

    added basic functions

---
+++ PyStackyMath.py
@@ -0,0 +1,12 @@
+from math import*
+class S(object):
+ def _init_(self):
+  self.stack=[]
+  self.fn=fn
+ def p(self):
+  return self.stack.pop(0)
+ def pu(self,v):
+  self.stack+=[val]
+ def e(self,code):
+  
+ 
